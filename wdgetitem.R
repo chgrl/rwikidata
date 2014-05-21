23b278396bc9945ea403dae5de3cc877033ab5e4
chgrl <christian.graul@gmail.com>
2014-05-21 13:07:43 +0200

    get random item if id not set

--- wdgetitem.R
+++ wdgetitem.R
@@ -6,6 +6,20 @@ wdgetitem <- function(id, ...) UseMethod("wdgetitem")
 # get wikidata item
 wdgetitem.default <- function(id, lang="en", print=TRUE) {
 	
+	if(missing(id)) { # get random item
+		url <- paste0("http://www.wikidata.org/wiki/Special:Statistics?uselang=en")
+		raw <- GET(url, config=add_headers("User-agent"="rwikidata"))
+		if(raw$status_code!=200) stop("error loading wikidata statistics")
+		stat <- httr::content(raw, as="text")
+		stat.list <- strsplit(stat, "\n")[[1]]
+		stat.line <- stat.list[grep("Content pages", stat.list)]
+		stat.line <- sub(".*Content pages</a></td><td class=\"mw-statistics-numbers\">", "", stat.line)
+		stat.line <- substr(stat.line, 1, 15)
+		stat <- as.numeric(gsub("[^0-9]", "", stat.line))
+		id <- sample(1:stat, 1)
+		cat("id not set - get random item Q", id, " ...\n\n", sep="")
+	}
+	
 	if(is.numeric(id)) id <- paste0("Q", id)
 			
 	# prepare request
