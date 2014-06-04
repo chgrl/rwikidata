9baadab97b4a1c57dcb9fcce8563505101bb4ac8
chgrl <christian.graul@gmail.com>
2014-06-04 11:25:36 +0200

    get and print multiple claims

--- R/wdclaimcontent.R
+++ R/wdclaimcontent.R
@@ -57,63 +57,71 @@ wdclaimcontent.int <- function(url, lang) {
 		
 	# parse
 	claim <- httr::content(raw, as="parsed")
-	if(length(claim$claims)==0) warning("claim not found") 
+	if(length(claim$claims)==0) warning("claim(s) not found") 
 	claim <- claim$claims
+	n.claims <- length(claim[[1]])
 	
-	# get info
-	guid <- claim[[1]][[1]]$id	# if qid and pid given - only first claim, there might be more than one
-	item <- wdgetitem(strsplit(claim[[1]][[1]]$id, "$", fixed=TRUE)[[1]][1], lang=lang, print=FALSE)$entities[[1]]$labels[[1]]$value
-	prop <- wdgetproperty(claim[[1]][[1]]$mainsnak$property, lang=lang, print=FALSE)[1]
-	type <- claim[[1]][[1]]$mainsnak$datatype
+	for(cl in 1:n.claims) {
 	
-	# get content
-	content <- NULL
-	if(type=="string" || type=="url") { # string
-		content <- claim[[1]][[1]]$mainsnak$datavalue$value
-	} else if(type=="wikibase-item") { # Wikidata item
-		item.id <- paste0("Q", claim[[1]][[1]]$mainsnak$datavalue$value$"numeric-id")
-		item.label <- wdgetitem(item.id, lang, print=FALSE)$entities[[1]]$labels[[1]]$value
-		content <- c(item.id, item.label)
-	} else if(type=="time") { # time
-		time <- claim[[1]][[1]]$mainsnak$datavalue$value$time
-		time <- gsub("+", "", time, fixed=TRUE)
-		time <- gsub("^0+", "", time)
-		if(substr(time, 1,1)=="-") time <- paste0("0", time)
-		content <- strptime(strsplit(time, "Z")[[1]][1], "%Y-%m-%dT%H:%M:%S")
-	} else if(type=="globe-coordinate") { # geographic coordinate
-		content <- c(claim[[1]][[1]]$mainsnak$datavalue$value$latitude, claim[[1]][[1]]$mainsnak$datavalue$value$longitude)
-	} else if(type=="commonsMedia") { # Wiki commons
-		file <- claim[[1]][[1]]$mainsnak$datavalue$value
-		# get commons website
-		url <- paste0("http://commons.wikimedia.org/wiki/File:", file, "?uselang=en")
-		url <- gsub(" ", "%20", url)
-		raw <- GET(url, config=add_headers("User-agent"="rwikidata"))
-		web <- httr::content(raw, as="text")
+		# get info
+		guid <- claim[[1]][[cl]]$id	# if qid and pid given - only first claim, there might be more than one
+		item <- wdgetitem(strsplit(claim[[1]][[cl]]$id, "$", fixed=TRUE)[[1]][1], lang=lang, print=FALSE)$entities[[1]]$labels[[1]]$value
+		prop <- wdgetproperty(claim[[1]][[cl]]$mainsnak$property, lang=lang, print=FALSE)[1]
+		type <- claim[[1]][[cl]]$mainsnak$datatype
 		
-		# get image url
-		web.list <- strsplit(web, "\n")[[1]]
-		img.url <- web.list[grep("fullMedia", web.list)]
-		#<div class=\"fullMedia\"><a href=\"//upload.wikimedia.org/wikipedia/commons/b/ba/Flag_of_Germany.svg\" class=\"internal\" title=\"Flag of Germany.svg\">Original file</a>
-		img.url <- sub(".*class=\"fullMedia\"><a href=\"", "", img.url)
-		img.url <- sub("\" class=\"internal\".*", "", img.url)
-		fname <- tail(strsplit(img.url, "/", fixed=TRUE)[[1]], 1)
+		# get content
+		content <- NULL
+		if(type=="string" || type=="url") { # string
+			content <- claim[[1]][[cl]]$mainsnak$datavalue$value
+		} else if(type=="wikibase-item") { # Wikidata item
+			item.id <- paste0("Q", claim[[1]][[cl]]$mainsnak$datavalue$value$"numeric-id")
+			item.label <- wdgetitem(item.id, lang, print=FALSE)$entities[[1]]$labels[[1]]$value
+			content <- c(item.id, item.label)
+		} else if(type=="time") { # time
+			time <- claim[[1]][[cl]]$mainsnak$datavalue$value$time
+			time <- gsub("+", "", time, fixed=TRUE)
+			time <- gsub("^0+", "", time)
+			if(substr(time, 1,1)=="-") time <- paste0("0", time)
+			content <- strptime(strsplit(time, "Z")[[1]][1], "%Y-%m-%dT%H:%M:%S")
+		} else if(type=="globe-coordinate") { # geographic coordinate
+			content <- c(claim[[1]][[cl]]$mainsnak$datavalue$value$latitude, claim[[1]][[cl]]$mainsnak$datavalue$value$longitude)
+		} else if(type=="commonsMedia") { # Wiki commons
+			file <- claim[[1]][[cl]]$mainsnak$datavalue$value
+			# get commons website
+			url <- paste0("http://commons.wikimedia.org/wiki/File:", file, "?uselang=en")
+			url <- gsub(" ", "%20", url)
+			raw <- GET(url, config=add_headers("User-agent"="rwikidata"))
+			web <- httr::content(raw, as="text")
+			
+			# get image url
+			web.list <- strsplit(web, "\n")[[1]]
+			img.url <- web.list[grep("fullMedia", web.list)]
+			#<div class=\"fullMedia\"><a href=\"//upload.wikimedia.org/wikipedia/commons/b/ba/Flag_of_Germany.svg\" class=\"internal\" title=\"Flag of Germany.svg\">Original file</a>
+			img.url <- sub(".*class=\"fullMedia\"><a href=\"", "", img.url)
+			img.url <- sub("\" class=\"internal\".*", "", img.url)
+			fname <- tail(strsplit(img.url, "/", fixed=TRUE)[[1]], 1)
+			
+			# download image and convert to png
+			download.file(url=paste0("http:", img.url), destfile=file.path(tempdir(), fname))
+			out <- paste0(file.path(getwd(), head(strsplit(fname, ".", fixed=TRUE)[[1]], -1)), ".png")
+			# ImageMagick required
+			if(tolower(tail(strsplit(fname, ".", fixed=TRUE)[[1]], 1))!="png") system(paste("convert", file.path(tempdir(), fname), out))
+			content <- out
+		} else if(type=="quantity") { # quantity
+			amount <- as.numeric(claim[[1]][[cl]]$mainsnak$datavalue$value$amount)
+			unit <- claim[[1]][[cl]]$mainsnak$datavalue$value$unit
+			content <- amount
+			attr(content, "unit") <- unit
+		}
 		
-		# download image and convert to png
-		download.file(url=paste0("http:", img.url), destfile=file.path(tempdir(), fname))
-		out <- paste0(file.path(getwd(), head(strsplit(fname, ".", fixed=TRUE)[[1]], -1)), ".png")
-		# ImageMagick required
-		if(tolower(tail(strsplit(fname, ".", fixed=TRUE)[[1]], 1))!="png") system(paste("convert", file.path(tempdir(), fname), out))
-		content <- out
-	} else if(type=="quantity") { # quantity
-		amount <- as.numeric(claim[[1]][[1]]$mainsnak$datavalue$value$amount)
-		unit <- claim[[1]][[1]]$mainsnak$datavalue$value$unit
-		content <- amount
-		attr(content, "unit") <- unit
+		if(n.claims>1) {
+			if(cl==1) wdcontent <- list(item=item, property=prop, claims=list())
+			wdcontent$claims[[cl]] <- list(guid=guid, type=type, content=content)
+		} else wdcontent <- list(guid=guid, item=item, property=prop, type=type, content=content)
 	}
 	
-	content <- list(guid=guid, item=item, property=prop, type=type, content=content)
-	class(content) <- "wdcontent"
-	return(content)
+	class(wdcontent) <- "wdcontent"
+	return(wdcontent)
 }
 
 
@@ -123,35 +131,73 @@ wdclaimcontent.int <- function(url, lang) {
 #' @param open.ext Logical - if \code{TRUE} external sources of the claim like images or URLs are opened
 print.wdcontent <- function(content, open.ext=TRUE) {
 	cat("\n\tWikidata claim content\n\n")
-	cat(paste("GUID:", content$guid, "\n"))
-	cat(paste("Item:", content$item, "\n"))
-	cat(paste("Property:", content$property, "\n"))
-	cat(paste("Type:", content$type, "\n\n"))
 	
-	if(content$type=="string") {
-		cat(content$content, "\n")
-	} else if(content$type=="time") {
-		cat(as.character(content$content), "\n")
-	} else if(content$type=="quantity") {
-		cat(content$content, attr(content$content, "unit"))
-	} else if(content$type=="url") {
-		cat(content$content, "\n")
-		if(open.ext) browseURL(content$content)
-	} else if(content$type=="wikibase-item") {
-		cat(content$content[1], "-", content$content[2], "\n")
-	} else if(content$type=="globe-coordinate") {
-		cat(content$content[1], ",", content$content[2], "\n", sep="")
-		if(open.ext && is.numeric(content$content[1]) && is.numeric(content$content[2])) browseURL(paste("http://www.openstreetmap.org/#map=10", content$content[1], content$content[2], sep="/"))
-	} else if(content$type=="commonsMedia") {
-		cat("PNG image:", content$content, "\n")
-		if(open.ext) {
-			stopifnot(require(png))
-			img <- readPNG(content$content)
-			dim.img <- dim(img)
-			dev.new(width=5*dim.img[2]/dim.img[1], height=5)
-			par(mar=c(0,0,0,0))
-			plot(c(0, dim.img[2]), c(0, dim.img[1]), type="n", xlab="", ylab="")
-			rasterImage(img, 1, 1, dim.img[2], dim.img[1])
+	if(is.null(content$claims)) {
+		cat(paste("GUID:", content$guid, "\n"))
+		cat(paste("Item:", content$item, "\n"))
+		cat(paste("Property:", content$property, "\n"))
+		cat(paste("Type:", content$type, "\n\n"))
+		
+		if(content$type=="string") {
+			cat(content$content, "\n")
+		} else if(content$type=="time") {
+			cat(as.character(content$content), "\n")
+		} else if(content$type=="quantity") {
+			cat(content$content, attr(content$content, "unit"))
+		} else if(content$type=="url") {
+			cat(content$content, "\n")
+			if(open.ext) browseURL(content$content)
+		} else if(content$type=="wikibase-item") {
+			cat(content$content[1], "-", content$content[2], "\n")
+		} else if(content$type=="globe-coordinate") {
+			cat(content$content[1], ",", content$content[2], "\n", sep="")
+			if(open.ext && is.numeric(content$content[1]) && is.numeric(content$content[2])) browseURL(paste("http://www.openstreetmap.org/#map=10", content$content[1], content$content[2], sep="/"))
+		} else if(content$type=="commonsMedia") {
+			cat("PNG image:", content$content, "\n")
+			if(open.ext) {
+				stopifnot(require(png))
+				img <- readPNG(content$content)
+				dim.img <- dim(img)
+				dev.new(width=5*dim.img[2]/dim.img[1], height=5)
+				par(mar=c(0,0,0,0))
+				plot(c(0, dim.img[2]), c(0, dim.img[1]), type="n", xlab="", ylab="")
+				rasterImage(img, 1, 1, dim.img[2], dim.img[1])
+			}
 		}
-	}
+	} else {
+		cat(paste("Item:", content$item, "\n"))
+		cat(paste("Property:", content$property, "\n"))
+		
+		for(i in 1:length(content$claims)) {
+			cat(paste("\n", i, "\nGUID:", content$claims[[i]]$guid, "\n"))
+			cat(paste("Type:", content$claims[[i]]$type, "\n"))
+			
+			if(content$claims[[i]]$type=="string") {
+				cat(content$claims[[i]]$content, "\n")
+			} else if(content$claims[[i]]$type=="time") {
+				cat(as.character(content$claims[[i]]$content), "\n")
+			} else if(content$claims[[i]]$type=="quantity") {
+				cat(content$claims[[i]]$content, attr(content$claims[[i]]$content, "unit"))
+			} else if(content$claims[[i]]$type=="url") {
+				cat(content$claims[[i]]$content, "\n")
+				if(open.ext) browseURL(content$claims[[i]]$content)
+			} else if(content$claims[[i]]$type=="wikibase-item") {
+				cat(content$claims[[i]]$content[1], "-", content$claims[[i]]$content[2], "\n")
+			} else if(content$claims[[i]]$type=="globe-coordinate") {
+				cat(content$claims[[i]]$content[1], ",", content$claims[[i]]$content[2], "\n", sep="")
+				if(open.ext && is.numeric(content$claims[[i]]$content[1]) && is.numeric(content$claims[[i]]$content[2])) browseURL(paste("http://www.openstreetmap.org/#map=10", content$claims[[i]]$content[1], content$claims[[i]]$content[2], sep="/"))
+			} else if(content$claims[[i]]$type=="commonsMedia") {
+				cat("PNG image:", content$claims[[i]]$content, "\n")
+				if(open.ext) {
+					stopifnot(require(png))
+					img <- readPNG(content$claims[[i]]$content)
+					dim.img <- dim(img)
+					dev.new(width=5*dim.img[2]/dim.img[1], height=5)
+					par(mar=c(0,0,0,0))
+					plot(c(0, dim.img[2]), c(0, dim.img[1]), type="n", xlab="", ylab="")
+					rasterImage(img, 1, 1, dim.img[2], dim.img[1])
+				}
+			}
+		}
+	}					
 }
