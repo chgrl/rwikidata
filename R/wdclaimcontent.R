26c97e271c15163e3e8b545d4cf5c9263c3651bd
chgrl <christian.graul@gmail.com>
2014-06-05 11:54:50 +0200

    claim qualifiers support added

--- R/wdclaimcontent.R
+++ R/wdclaimcontent.R
@@ -16,6 +16,9 @@
 #' wdclaimcontent(qid=144786, pid=17)
 #' wdclaimcontent(guid="q144786$258996D0-319A-4A14-971A-6F3326C05ADD")  # same same
 #'
+#' # with qualifiers
+#' wdclaimcontent(183, 36)
+#'
 #' # different content types
 #' wdclaimcontent(guid="q206904$26C11CAB-2FEE-4A03-9774-9B70777616B3")  # wikidata item
 #' wdclaimcontent(guid="q52$61ba10af-455f-021c-19d5-d1a0df4d65f5")  # date/time
@@ -47,6 +50,7 @@ wdclaimcontent <- function(qid, pid, guid, lang="en", print=TRUE, ...) {
 #' Internal method to execute API request and process the content
 #'
 #' @param url API request URL
+#' @param lang Language abbreviation (ISO language codes), as string
 #' @return A list of guid, item, property, type and content
 #' @seealso \code{\link{wdclaimcontent}}
 #' @keywords internal
@@ -114,14 +118,68 @@ wdclaimcontent.int <- function(url, lang) {
 			attr(content, "unit") <- unit
 		}
 		
+		# qualifiers
+		qualifiers <- list()
+		if(!is.null(claim[[1]][[cl]]$qualifiers)) {
+			qualifiers <- claim[[1]][[cl]]$qualifiers
+			n.qual <- length(qualifiers)
+			for(i in 1:n.qual) {
+				### todo: qualifier might be a list - qualifiers[[i]][[x]]
+				qual.prop <- wdgetproperty(qualifiers[[i]][[1]]$property, lang=lang, print=FALSE)[1]
+				qual.type <- qualifiers[[i]][[1]]$datatype
+				if(!is.null(qualifiers[[i]][[1]]$datavalue)) qualifiers[[i]] <- getqualifier(qualifiers[[i]][[1]]$datavalue, qual.prop, qual.type, lang)
+			}
+		}
+		
 		if(n.claims>1) {
 			if(cl==1) wdcontent <- list(item=item, property=prop, claims=list())
-			wdcontent$claims[[cl]] <- list(guid=guid, type=type, content=content)
-		} else wdcontent <- list(guid=guid, item=item, property=prop, type=type, content=content)
+			if(length(qualifiers)==0) wdcontent$claims[[cl]] <- list(guid=guid, type=type, content=content)
+			else wdcontent$claims[[cl]] <- list(guid=guid, type=type, qualifiers=qualifiers, content=content)
+		} else {
+			if(length(qualifiers)==0) wdcontent <- list(guid=guid, item=item, property=prop, type=type, content=content)
+			else wdcontent <- list(guid=guid, item=item, property=prop, type=type, qualifiers=qualifiers, content=content)
+		}
 	}
 	
 	class(wdcontent) <- "wdcontent"
 	return(wdcontent)
+}
+
+
+#' Internal method to get and prepare the qualifiers
+#'
+#' @param qualifier Raw qualifier object
+#' @param lang Language abbreviation (ISO language codes), as string
+#' @return Qualifier value - object depends on qualifier type
+#' @seealso \code{\link{wdclaimcontent.int}}
+#' @keywords internal
+getqualifier <- function(qualifier, prop, type, lang) {
+	if(type=="string") {
+		value <- qualifier$value
+		attr(value, "type") <- "string"
+	} else if(type=="time") {
+		time <- qualifier$value$time
+		time <- gsub("+", "", time, fixed=TRUE)
+		time <- gsub("^0+", "", time)
+		if(substr(time, 1,1)=="-") time <- paste0("0", time)
+		value <- strptime(strsplit(time, "Z")[[1]][1], "%Y-%m-%dT%H:%M:%S")
+		attr(value, "type") <- "time"
+	} else if(type=="quantity") {
+		value <- as.numeric(qualifier$value$amount)
+		attr(value, "unit") <- qualifier$value$unit
+		attr(value, "type") <- "quantity"
+	} else if(type=="wikibase-item") {
+		item.id <- paste0("Q", qualifier$value$"numeric-id")
+		item.label <- wdgetitem(item.id, lang, print=FALSE)$entities[[1]]$labels[[1]]$value
+		value <- c(item.id, item.label)
+		attr(value, "type") <- "wikibase-item"
+	} else if(type=="globe-coordinate") {
+		value <- c(qualifier$value$latitude, qualifier$value$longitude)
+		attr(value, "type") <- "globe-coordinate"
+	} else warning("qualifier not recognized", call.=FALSE)
+	
+	attr(value, "property") <- prop
+	return(value)
 }
 
 
@@ -132,12 +190,13 @@ wdclaimcontent.int <- function(url, lang) {
 print.wdcontent <- function(content, open.ext=TRUE) {
 	cat("\n\tWikidata claim content\n\n")
 	
-	if(is.null(content$claims)) {
+	if(is.null(content$claims)) { # single claim content
 		cat(paste("GUID:", content$guid, "\n"))
 		cat(paste("Item:", content$item, "\n"))
 		cat(paste("Property:", content$property, "\n"))
 		cat(paste("Type:", content$type, "\n\n"))
 		
+		# content
 		if(content$type=="string") {
 			cat(content$content, "\n")
 		} else if(content$type=="time") {
@@ -164,10 +223,14 @@ print.wdcontent <- function(content, open.ext=TRUE) {
 				rasterImage(img, 1, 1, dim.img[2], dim.img[1])
 			}
 		}
-	} else {
+		
+		# qualifiers
+		# ...
+	} else { # multiple claim content
 		cat(paste("Item:", content$item, "\n"))
 		cat(paste("Property:", content$property, "\n"))
 		
+		# content
 		for(i in 1:length(content$claims)) {
 			cat(paste("\n", i, "\nGUID:", content$claims[[i]]$guid, "\n"))
 			cat(paste("Type:", content$claims[[i]]$type, "\n"))
@@ -199,5 +262,8 @@ print.wdcontent <- function(content, open.ext=TRUE) {
 				}
 			}
 		}
+		
+		# qualifiers
+		# ...
 	}					
 }
