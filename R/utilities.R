de5da5d18d45ef07f18c5a02bdd487fdc8d6c198
chgrl <christian.graul@gmail.com>
2015-02-05 21:21:55 +0100

    documentation updated

--- R/utilities.R
+++ R/utilities.R
@@ -1,7 +1,13 @@
-#'@importFrom httr GET content stop_for_status add_headers
+#' Internal method to execute API request and return the content
+#'
+#' @importFrom httr GET content stop_for_status add_headers
+#' @param url API request URL
+#' @param as_what Desired type of output, one of \code{raw}, \code{text} or \code{parsed.content}
+#' @return Content in specified type
+#' @keywords internal
 query <- function(url, as_what){
-  result <- GET(url, config=add_headers("User-agent"="rwikidata"))
-  stop_for_status(result)
-  result <- content(result, as = as_what)
-  return(result)
+	result <- GET(url, config=add_headers("User-agent"="rwikidata"))
+	stop_for_status(result)
+	result <- content(result, as=as_what)
+	return(result)
 }
